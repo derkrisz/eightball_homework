810ff42d1ee88fa5a6d68029ca7556cab8d801ff
derkrisz <krisztian.dergmail.com>
2017-12-04 18:40:30 +0000

    empty array test passes

--- src/main/java/EightBall.java
+++ src/main/java/EightBall.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class EightBall {
+    ArrayList<String> sentences;
+
+    public EightBall() {
+        this.sentences = new ArrayList<>();
+    }
+
+    public int getWordCount() {
+        return this.sentences.size();
+    }
 }
--- src/test/java/TestEightBall.java
+++ src/test/java/TestEightBall.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestEightBall {
+    EightBall eightBall;
+
+    @Before
+    public void before() {
+        eightBall = new EightBall();
+    }
+
+    @Test
+    public void hasNoEntries() {
+        assertEquals(0, eightBall.getWordCount());
+    }
 }
